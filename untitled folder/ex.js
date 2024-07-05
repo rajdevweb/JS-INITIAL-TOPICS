71d730e5f4f3b018a3477eefd1d5a53d1a2caebc
rajdevweb <wordhello160@gmail.com>
2024-07-05 23:16:20 +0530

    done

---
+++ untitled folder/ex.js
@@ -0,0 +1,9 @@
+console.log("hello extrernal js")
+document.write("hello javascript","<br>")
+
+// bom pop box
+// window.alert("invlid vaue")
+// window.prompt("5 + 5")
+// window.confirm("are your sure ?")
+// window.print()
+document.write("sagugb")
