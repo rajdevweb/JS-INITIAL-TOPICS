f793a94bc7d2c5fa9b3f2d5ed26e957c3e8cac34
rajdevweb <wordhello160@gmail.com>
2024-10-18 12:28:24 +0530

    explain new and this keyword in javascript

---
+++ 35_OOPS/object.js
@@ -0,0 +1,31 @@
+//program 1
+// function multiple5(num){
+//     return num * 5;
+// }
+// multiple5.power = 2
+
+// console.log(multiple5(1));
+// console.log(multiple5.power);
+// console.log(multiple5.prototype);
+
+//program 2
+//using this keyword and it explain 
+function createUser(username , salary){
+    this.username = username;
+    this.salary = salary
+}
+
+createUser.prototype.increment = function() {
+  this.salary++ 
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`salary is ${this.salary}`);
+    
+}
+
+let a = createUser("rahul",10000)
+// let b = createUser("rahul","$100")
+console.log(a);
+
+
