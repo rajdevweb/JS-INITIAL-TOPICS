a29b74e1df22102501505e261d704440ad65cb3d
rajdevweb <wordhello160@gmail.com>
2024-10-20 20:32:14 +0530

    react

---
+++ react/script.js
@@ -0,0 +1,18 @@
+// let h1 = document.createElement("h1").innerHTML = "hello my name is raj vishwakarma and i am parsuig bca and i am student of silver oak university. i am from nepal. nepal is beatiful place all around nature and nature. nature is better for living thing. earth's leaving this are destroy of mother's earth. earth is very amazing and one of the exitance life planet, here are 9 planet in our solar system"
+
+
+// var root = document.querySelector("#root").innerHTML = "hello from js "
+
+// root.appendChild(h1)
+
+console.log(react);
+
+
+
+
+
+
+
+
+
+
